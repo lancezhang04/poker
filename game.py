bdcab576a49d3cb8bcaba1e7f6cbef22d3ad864c
Lance Zhang <lzhang26@utexas.edu>
2024-11-01 16:10:19 -0500

    added one-on-one flop game mode

--- game.py
+++ game.py
@@ -67,28 +67,98 @@ class Game:
     def river(self) -> List[Card]:
         return self.deal_card(4)
 
-    def cur_winner(self):
-        print(f"Player 0 type: {self.player_hands[0].type}, order: {self.player_hands[0].order}")
-        print(f"Player 1 type: {self.player_hands[1].type}, order: {self.player_hands[1].order}")
+    def cur_winner(self, verbose=False) -> int:
+        if verbose:
+            print(f"Player 0 type: {self.player_hands[0].type}, order: {self.player_hands[0].order}")
+            print(f"Player 1 type: {self.player_hands[1].type}, order: {self.player_hands[1].order}")
         if self.player_hands[0] > self.player_hands[1]:
-            print("Player 0 is winning\n")
+            if verbose:
+                print("Player 0 is winning\n")
+            return 0
         elif self.player_hands[0] < self.player_hands[1]:
-            print("Player 1 is winning\n")
+            if verbose:
+                print("Player 1 is winning\n")
+            return 1
         else:
-            print("Players are tied\n")
+            if verbose:
+                print("Players are tied\n")
+            return 2
 
-    def play_simple(self):
-        total = 0
-        wins = 0
-        while (True):
-            pass
+def play_flop():
+    """
+    0: folded, would have won
+    1: folded, would have lost
+    2: folded, would have drawn
+    3: bet, won
+    4: bet, lost
+    5: best, drew
+    """
+    stats: List[int] = [0, 0, 0, 0, 0, 0]
+    balance: int = 0
+
+    num_rounds: int = -1
+    while num_rounds == -1:
+        try:
+            num_rounds = int(input("How many rounds would you like to play?  >> "))
+        except ValueError:
+            print("Please enter an integer\n")
+    print()
+
+    for i in range(num_rounds):
+        print(f"Round {i + 1} out of {num_rounds}")
+        game = Game()
+        game.deal_starting_cards()
+        print("Flop:")
+        for card in game.community_cards:
+            print("  " + str(card))
+        print("Hand:")
+        for card in game.player_hands[0].hole_cards:
+            print("  " + str(card))
+        print()
+
+        ans = ""
+        while ans not in ["fold", "bet"]:
+            ans = input("Would you like to \"fold\" or \"bet\"?  >> ")
+        print()
+
+        game.turn()
+        game.river()
+        winner = game.cur_winner()
+
+        if ans == "fold":
+            stats[winner] += 1
+        if ans == "bet":
+            stats[3 + winner] += 1
+            balance -= 1
+            if winner == 0:
+                balance += 2
+            elif winner == 2:
+                balance += 1
+
+        # print current stats and credits after each round
+        print("Current Stats:")
+        print(f"Folded, would have won: {stats[0]}")
+        print(f"Folded, would have lost: {stats[1]}")
+        print(f"Folded, would have drawn: {stats[2]}")
+        print(f"Bet, won: {stats[3]}")
+        print(f"Bet, lost: {stats[4]}")
+        print(f"Bet, drew: {stats[5]}")
+        balance_str = str(balance)
+        if balance > 0:
+            balance_str = "+" + balance_str
+        elif balance < 0:
+            balance_str = "-" + balance_str
+        print(f"Balance: {balance_str}")
+        print()
 
 
 if __name__ == "__main__":
-    game: Game = Game(verbose=True)
-    game.deal_starting_cards()
-    game.cur_winner()
-    game.turn()
-    game.cur_winner()
-    game.river()
-    game.cur_winner()
+    # game: Game = Game(verbose=True)
+    # game.deal_starting_cards()
+    # game.cur_winner(verbose=True)
+    # game.turn()
+    # game.cur_winner(verbose=True)
+    # game.river()
+    # game.cur_winner(verbose=True)
+
+    play_flop()
