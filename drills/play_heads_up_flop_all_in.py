a080a2c73430c65991018acf0210e15a25ac9dfa
Lance Zhang <lzhang26@utexas.edu>
2024-11-14 13:39:37 -0600

    updated heads up flop drill with monte carlo simulation

--- drills/play_heads_up_flop_all_in.py
+++ drills/play_heads_up_flop_all_in.py
@@ -1,6 +1,10 @@
 from typing import List
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 from poker.game import Game
+from poker.deck import Deck
+from poker.hand import Hand
 
 
 def play_flop(simulate=True):
@@ -25,67 +29,125 @@ def play_flop(simulate=True):
 
     for i in range(num_rounds):
         print(f"Round {i + 1} out of {num_rounds}")
-        game = Game()
+        game = Game(num_players=2)
         game.deal_starting_cards()
+        game.flop()
         print("Flop:")
         for card in game.community_cards:
             print("  " + str(card))
-        print("Hand:")
+        print("Hole cards:")
         for card in game.player_hands[0].hole_cards:
             print("  " + str(card))
         print()
 
         ans = ""
-        while ans not in ["fold", "bet"]:
-            ans = input("Would you like to \"fold\" or \"bet\"?  >> ")
-        print()
-
-        game.turn()
-        game.river()
-        winner = game.cur_winner()
+        while ans not in ["fold", "bet", "f", "b"]:
+            ans = input("Would you like to fold (\"f\") or bet (\"b\")?  >> ")
+            if ans.lower() == "quit":
+                exit(0)
 
         if simulate:
-            win_prob = simulate_win_prob(game.player_hands[0].hole_cards)
+            win_prob = simulate_win_prob(
+                game.player_hands[0].hole_cards,
+                game.community_cards[:3]
+            )
             print(f"Simulated win probability: {win_prob * 100:.2f}%")
+            if win_prob < 0.5:
+                print("Folding was the correct decision!")
+                if ans in ["fold", "f"]:
+                    balance += 1
+            else:
+                print("Betting was the correct decision!")
+                if ans in ["bet", "b"]:
+                    balance += 1
+            print(f"Record: {balance}/{i + 1} or {balance / (i + 1) * 100:.2f}%\n")
+        else:
+            game.turn()
+            game.river()
+            winner = game.cur_winner()
 
-        if ans == "fold":
-            stats[winner] += 1
-        if ans == "bet":
-            stats[3 + winner] += 1
-            balance -= 1
-            if winner == 0:
-                balance += 2
-            elif winner == 2:
-                balance += 1
+            if ans in ["fold", "f"]:
+                stats[winner] += 1
+            if ans in ["bet", "b"]:
+                stats[3 + winner] += 1
+                balance -= 1
+                if winner == 0:
+                    balance += 2
+                elif winner == 2:
+                    balance += 1
 
-        # print current stats and credits after each round
-        print("Current Stats:")
-        print(f"Folded, would have won: {stats[0]}")
-        print(f"Folded, would have lost: {stats[1]}")
-        print(f"Folded, would have drawn: {stats[2]}")
-        print(f"Bet, won: {stats[3]}")
-        print(f"Bet, lost: {stats[4]}")
-        print(f"Bet, drew: {stats[5]}")
-        balance_str = str(balance)
-        if balance > 0:
-            balance_str = "+" + balance_str
-        elif balance < 0:
-            balance_str = "-" + balance_str
-        print(f"Balance: {balance_str}")
-        print()
+            # print current stats and credits after each round
+            print("\nCurrent Stats:")
+            print(f"Folded, would have won: {stats[0]}")
+            print(f"Folded, would have lost: {stats[1]}")
+            print(f"Folded, would have drawn: {stats[2]}")
+            print(f"Bet, won: {stats[3]}")
+            print(f"Bet, lost: {stats[4]}")
+            print(f"Bet, drew: {stats[5]}")
+            balance_str = str(balance)
+            if balance > 0:
+                balance_str = "+" + balance_str
+            print(f"Balance: {balance_str}")
+            print()
 
-def simulate_win_prob(hole_cards, num_iters=100):
+def simulate_win_prob(hole_cards, flop_cards, num_iters=5000, show_plot=False, verbose=False):
     wins = 0
-    for _ in tqdm(range(num_iters), ncols=80):
+    data = [list(), list()]
+
+    for i in tqdm(range(num_iters), ncols=80):
         game = Game()
-        game.deal_starting_cards()
-        game.player_hands[0].hole_cards = hole_cards
+        game.deck = Deck(game.full_decks)
+        for card in hole_cards + flop_cards:
+            game.deck.remove_card(card)
+
+        game.player_hands.append(Hand(
+            flop_cards.copy(),
+            hole_cards.copy()
+        ))
+        game.player_hands.append(Hand(
+            flop_cards.copy(),
+            [game.deck.draw_card() for _ in range(2)]
+        ))
+        game.community_cards = flop_cards.copy()
+
         game.turn()
         game.river()
         if game.cur_winner() == 0:
             wins += 1
+
+        if verbose:
+            print("Hero hole cards:")
+            for card in game.player_hands[1].community_cards:
+                print(card)
+            print("Villain hole cards:")
+            for card in game.player_hands[1].hole_cards:
+                print(card)
+            game.cur_winner(verbose=True)
+            print()
+
+        if show_plot and (i + 1) % 100 == 0:
+            data[0].append(i + 1)
+            data[1].append(wins / (i + 1))
+
+    if show_plot:
+        plt.plot(data[0], data[1], label=f"Final: {wins / num_iters * 100:.2f}%")
+        plt.ylim([0, 1])
+        plt.legend()
+        plt.show()
     return wins / num_iters
 
 
 if __name__ == "__main__":
     play_flop()
+
+    # from poker.card import Card, Suit
+    # comm_cards = [
+    #     Card(Suit.HEARTS, 11),
+    #     Card(Suit.DIAMONDS, 5),
+    #     Card(Suit.SPADES, 5),
+    # ]
+    # hole_cards = [
+    #     Card(Suit.CLUBS, 11),
+    #     Card(Suit.HEARTS, 6),
+    # ]
+    # print(simulate_win_prob(hole_cards, comm_cards, num_iters=10000))
